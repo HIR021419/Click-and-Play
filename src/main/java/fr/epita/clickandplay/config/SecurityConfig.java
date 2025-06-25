c00b85445e907f7cf9f8ccfeb5b92e470ac26101
HIR021419 <yann.lemetayer@epita.fr>
2025-06-25 20:54:47 +0200

    feat: security config + user details service

---
+++ src/main/java/fr/epita/clickandplay/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package fr.epita.clickandplay.config;
+
+import fr.epita.clickandplay.service.CustomUserDetailsService;
+import org.hibernate.service.spi.InjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/sessions/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.maximumSessions(1))
+                .userDetailsService(userDetailsService)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/fr/epita/clickandplay/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package fr.epita.clickandplay.service;
+
+import fr.epita.clickandplay.model.User;
+import fr.epita.clickandplay.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private IUserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("ROLE_" + user.getRole().name())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
