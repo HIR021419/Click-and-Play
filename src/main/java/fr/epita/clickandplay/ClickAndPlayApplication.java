023ee61f2c23540190f77303cd267bcd5f70b298
HIR021419 <yann.lemetayer@epita.fr>
2025-06-25 18:20:13 +0200

    initial push

---
+++ src/main/java/fr/epita/clickandplay/ClickAndPlayApplication.java
@@ -0,0 +1,13 @@
+package fr.epita.clickandplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickAndPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickAndPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epita/clickandplay/ClickAndPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.clickandplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickAndPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
